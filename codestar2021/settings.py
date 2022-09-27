dcf71ced95e0a406b971e3120da2218548404a82
burkero2 <ronanburke@codeinstitute.net>
2022-09-27 11:38:56 +0000

    Deployment Commit

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["rb-codestar-2021.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["rb-codestar-2021.herokuapp.com", "localhost"]
 
 
 # Application definition
